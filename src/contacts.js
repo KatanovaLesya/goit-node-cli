2332b1e70776e5268cd0c1f49b72be90ba6a3160
KatanovaLesya <katanovalesya@gmail.com>
2025-06-16 10:14:17 +0300

    +

--- src/contacts.js
+++ src/contacts.js
@@ -8,20 +8,20 @@ const updateContact = contacts => fs.writeFile(contactsPath, JSON.stringify(cont
 
 console.log(contactsPath);
 
-export const getContacts = async () => {
+export const listContacts = async () => {
     const data = await fs.readFile(contactsPath, 'utf-8');
     return JSON.parse(data);
 }
 
 export const getContactById = async (id) => {
-    const contact = await getContacts();
+    const contact = await listContacts();
     const result = contact.find(item => item.id === id);
     return result || null;
     
 }
 
 export const addContact = async data => {
-    const contacts = await getContacts();
+    const contacts = await listContacts();
     const newContact = {
         id: nanoid(),
         ...data,
@@ -31,17 +31,17 @@ export const addContact = async data => {
     return newContact;
 }
 
-export const updateContactById = async (id, data) => {
-    const contacts = await getContacts();
-    const index = contacts.findIndex(item => item.id === id);
-    if (index === -1) return null;
-    contacts[index] = {...contacts[index],...data,};
-    await updateContact(contacts);
-    return contacts[index];
-}
+//export const updateContactById = async (id, data) => {
+    //const contacts = await listContacts();
+    //const index = contacts.findIndex(item => item.id === id);
+    //if (index === -1) return null;
+    //contacts[index] = {...contacts[index],...data,};
+    //await updateContact(contacts);
+    //return contacts[index];
+//}
 
-export const deleteContactById = async id => {
-    const contacts = await getContacts();
+export const removeContact = async id => {
+    const contacts = await listContacts();
     const index = contacts.findIndex(item => item.id === id);
     if (index === -1) return null;
     const [deletedContact] = contacts.splice(index, 1);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import { program } from "commander";
-import { getContacts, getContactById, addContact, updateContactById, deleteContactById } from "./contacts.js";
+import { listContacts, getContactById, addContact, removeContact } from "./contacts.js";
 
 program
-  .option("-a, --action <type>")
-  .option("-i, --id <type>")
-  .option("-n, --name <type>")
-  .option("-e, --email <type>")
-  .option("-p, --phone <type>");
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
 
 program.parse(); //читаємо, робимо з масиву обʼєкт, але нічого не повертаємо
 
@@ -16,35 +16,34 @@ console.log(options);
 async function invokeAction({action, id, ...data}) {
   switch (action) {
     case "list":
-      const allContacts = await getContacts();
+      const allContacts = await listContacts();
       return console.table(allContacts);
 
-    case "getById":
+    case "get":
       const oneContact = await getContactById(id);
       return console.log(oneContact);
 
-    case "addContact":
+    case "add":
       const newContact = await addContact(data);
       return console.log(newContact);
     
-    case "updateById":
-      const updatedContact = await updateContactById(id, data);
-      return console.log(updatedContact);
+    //case "updateById":
+      //const updatedContact = await updateContactById(id, data);
+      //return console.log(updatedContact);
       
 
-    case "deleteById":
-      const deletedContact = await deleteContactById(id);
+    case "remove":
+      const deletedContact = await removeContact(id);
       return console.log(deletedContact);
 
     default:
-      //console.warn("\x1B[31m Unknown action type!");
-      console.log("немає");
+      console.warn("\x1B[31m Unknown action type!");
   }
 }
 //invokeAction({action: "list"});
-//invokeAction({action: "getById", id: "AeHIrLTr6JkxGE6SN-0Rw1"});
-//invokeAction({action: "addContact", name: "Igor", email: "igor@gmail.com", phone: "+380663456789"});
+//invokeAction({action: "get", id: "AeHIrLTr6JkxGE6SN-0Rw1"});
+//invokeAction({action: "add", name: "Igor2", email: "igor@gmail.com", phone: "+380663456789"});
 //invokeAction({action: "updateById", id: "YwrYjnKcgVB-p7CsEOpa1", name: "Igor3", email: "igor@gmail.com", phone: "+380663456789"});
 //invokeAction({action: "updateById", id: "YwrYjnKcgVB-p7CsEOpa1", name: "Igor4"});
-//invokeAction({action: "deleteById", id: "YwrYjnKcgVB-p7CsEOpa1"});
+//invokeAction({action: "remove", id: "nNo1az-c8LLYVzaEK5uu4"});
 invokeAction(options);
